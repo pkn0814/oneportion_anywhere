9319e5cd7f166c746cf970a59f4006b51305220e
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2021-07-23 23:45:02 +0900

    login signup ing2

--- accounts/views.py
+++ accounts/views.py
@@ -11,7 +11,7 @@ def signup_view(request):
         if form.is_valid():
             user = form.save()
             login(request, user)
-            return redirect("main")
+        return redirect("main")
     else:
         form = SignupForm()
         return render(request, 'signup.html',{'form':form})
