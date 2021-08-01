b0a5dcfdf340a1efc24ea3ce75e7b8d5557f9fe4
bestlalala <82032452+bestlalala@users.noreply.github.com>
2021-08-01 19:33:29 +0900

    edit myfridge

--- fridge/views.py
+++ fridge/views.py
@@ -1,6 +1,6 @@
 from fridge.models import Dish
 from django.shortcuts import redirect, render
-
+from django.db.models import Q 
 
 # Create your views here.
 def myfridge(request):
@@ -8,16 +8,19 @@ def myfridge(request):
 
 def showdish(request):
     maindish = Dish.objects.all()
+    adddish = Dish.objects.all()
     selectedMain = request.GET.getlist('ingredients')
+    selectedAdd = request.GET.getlist('additional')
+
     if selectedMain:
         for i in selectedMain:
             maindish = maindish.filter(main__icontains=i)
+            for k in selectedAdd:
+                maindish = maindish.filter(add__icontains=k)
     
-    adddish = Dish.objects.all()
-    selectedAdd = request.GET.getlist('additional')
     if selectedAdd:
         for k in selectedAdd:
-            adddish = adddish.filter(main__icontains=k)
+            adddish = adddish.filter(add__icontains=k)
     
-    return render(request, 'showdish.html', {'dishs': maindish, 'selectedMain':selectedMain, 'selectedAdd' : selectedAdd})
+    return render(request, 'showdish.html', {'maindish': maindish, 'adddish' : adddish, 'selectedMain':selectedMain, 'selectedAdd' : selectedAdd})
     
