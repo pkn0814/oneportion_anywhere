13a430f2dc91379f766fbcb82219ce22d3a5176d
bestlalala <82032452+bestlalala@users.noreply.github.com>
2021-08-03 21:32:33 +0900

    edit showdish view

--- fridge/views.py
+++ fridge/views.py
@@ -15,19 +15,17 @@ def showdish(request):
     if selectedMain :
 
         for i in selectedMain:
-            maindish = maindish.filter(main = i)
+            maindish = maindish.filter(main__icontains= i)
             adddish = adddish.filter(main__icontains = i)
 
             if selectedAdd:
                 for k in selectedAdd:
                     maindish = maindish.filter(add__icontains = k)
-                    adddish = adddish.filter(add__icontains = k) 
             else :
-                adddish = adddish.filter(add__isnull = True) 
+                adddish = adddish.filter(add__isnull = False) 
             
             maindish = maindish.order_by('main', 'add')
             adddish = adddish.order_by('main', 'add')
-
 
     return render(request, 'showdish.html', {'maindish': maindish, 'adddish' : adddish, 'selectedMain':selectedMain, 'selectedAdd' : selectedAdd })
     
