dcfea0ef645914dd036d465185b7b5b1ff8d977b
unknown <00jungwoon@naver.com>
2021-08-03 13:49:43 +0900

    commit

--- search/views.py
+++ search/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 from community.models import Post
+from expert.models import Expert
 # Create your views here.
 
 def result(request):
     post_object = Post.objects.all()
+    expert_object = Expert.objects.all()
     query = request.GET.get('query', '')
+
     if query:
         result = post_object.filter (title__contains=query) | post_object.filter(content__contains = query)
-    return render(request, 'result.html', {'result': result})
+        result2 = expert_object.filter (title__contains = query) | expert_object.filter(body__contains = query)
+    return render(request, 'result.html', {'result': result, 'result2':result2})
 
 def home(request):
     posts = Post.objects
