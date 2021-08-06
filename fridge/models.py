00b80c3d3f2008d798249d2ccbb870ef2ddfd171
bestlalala <82032452+bestlalala@users.noreply.github.com>
2021-08-07 00:49:17 +0900

    edit fridge/migrations

--- fridge/models.py
+++ fridge/models.py
@@ -4,8 +4,8 @@ class Dish(models.Model):
     name = models.CharField(max_length=15, default='') #음식 이름
     intro = models.TextField(max_length=30, default='') #음식 간단 소개
     image = models.ImageField(upload_to = 'images/', default='', blank = True, null=True) #음식 사진
-    main = models.CharField(max_length=100, default='') #메인재료
-    add = models.CharField(max_length=100, default='') #부가재료
+    basic = models.TextField(max_length=200, default='', blank = True, null=True) #기본재료 중에서 필요한 것
+    ingredients = models.TextField(max_length=300, default='') #필요한 재료
     option = models.CharField(max_length=100, default='', blank = True, null = True) #옵션
 
     def __str__(self):
