9370441ae8fdc9e4a7b764678c66dced9071c1ff
bestlalala <82032452+bestlalala@users.noreply.github.com>
2021-08-08 14:44:18 +0900

    fridge image

--- fridge/models.py
+++ fridge/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Dish(models.Model):
     name = models.CharField(max_length=15, default='') #음식 이름
     intro = models.TextField(max_length=30, default='') #음식 간단 소개
-    image = models.ImageField(upload_to = 'images/', default='', blank = True, null=True) #음식 사진
+    image = models.ImageField(upload_to = 'images/', default='loaddish.jpg', blank = True, null=True) #음식 사진
     basic = models.TextField(max_length=200, default='', blank = True, null=True) #기본재료 중에서 필요한 것
     ingredients = models.TextField(max_length=300, default='') #필요한 재료
     option = models.CharField(max_length=100, default='', blank = True, null = True) #옵션
