47154e46c1a1202adde37ab9ae1e548474534d79
bestlalala <82032452+bestlalala@users.noreply.github.com>
2021-08-09 22:02:40 +0900

    edit myfridge

--- fridge/views.py
+++ fridge/views.py
@@ -8,11 +8,11 @@ def myfridge(request):
 
 @require_GET
 def showdish(request):
-    allingre = ['밥', '식빵', '달걀', '김치', '햄', '라면', '소면', '파스타면', '두부면',
+    allingre = ['밥', '식빵', '달걀', '김치', '햄', '라면', '소면', '파스타면', '칼국수 면', '두부면',
         '닭고기', '닭가슴살', '돼지고기', '삼겹살', '돼지목살', '소고기', '차돌박이',
-        '버섯', '파', '양파', '샐러드야채', '시금치', '숙주나물', '콩나물',
-        '고추장', '고춧가루', '우유', '잼', '토마토소스', '까르보나라소스', '간장', '생크림', '굴소스', '치킨스톡', '통마늘', '다진마늘', '맛술', '불닭소스',
-        '딸기청', '자몽청', '플레인요거트'
+        '버섯', '파', '양파', '샐러드야채', '시금치', '숙주나물', '콩나물', '당근', '애호박', '고추', '깻잎', '청경채',
+        '고추장', '고춧가루', '우유', '잼', '토마토소스', '까르보나라소스', '생크림', '굴소스', '치킨스톡', '통마늘', '다진마늘', '맛술', '불닭소스', '마요네즈', '케찹', '버터',
+        '블루베리', '딸기', '바나나', '귤', '사과', '딸기청', '자몽청', '플레인요거트', '그릭요거트', '그래놀라', '시리얼'
         ]
     selected = request.GET.getlist('ingredients', None)
     searched = request.GET.getlist('direct', None)
@@ -36,9 +36,9 @@ def showdish(request):
                 qm &= Q(ingredients__icontains=i) #선택된 재료 모두를 포함하는 요리들
                 q |= Q(ingredients__icontains=i)    #선택된 재료 중 하나라도 포함하는 요리들
 
-        for ingre in allingre:
-            if i == ingre:
-                allingre.remove(ingre) # 선택하지 않은 재료 = 전체 재료 - 선택한 재료  
+            for ingre in allingre:
+                if i == ingre:
+                    allingre.remove(ingre) # 선택하지 않은 재료 = 전체 재료 - 선택한 재료  
 
         for j in allingre:  # 선택하지 않은 재료를 포함하는 요리 제외
             if j == allingre[0]:
