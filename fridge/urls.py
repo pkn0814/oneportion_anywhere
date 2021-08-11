40e4cc87cb7ea4cd041d145bba30234be9e844dc
bestlalala <82032452+bestlalala@users.noreply.github.com>
2021-08-11 11:48:29 +0900

    main random todays's dish success!

--- fridge/urls.py
+++ fridge/urls.py
@@ -6,4 +6,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.myfridge, name='myfridge'),
     path('showdish/', views.showdish, name='showdish'),
+    path('showdish/recipy/<int:dish_id>', views.recipy, name="recipy")
 ]
