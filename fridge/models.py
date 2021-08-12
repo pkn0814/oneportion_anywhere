53a4a2626c09cdd200f58f81c94407ff2e12937f
KyeongNa Park <61577469+pkn0814@users.noreply.github.com>
2021-08-12 18:59:38 +0900

    오노

--- fridge/models.py
+++ fridge/models.py
@@ -6,7 +6,7 @@ class Dish(models.Model):
     name = models.CharField(max_length=15, default='') #음식 이름
     intro = models.TextField(max_length=30, default='') #음식 간단 소개
     image = CloudinaryField('images/',default='ttgaogawdwdfmexebiwz.jpg',blank=True,null=False)
-    basic = models.TextField(max_length=200, default='', blank = True, null=True) #기본재료 중에서 필요한 것
+    basic = models.TextField(max_length=200, default='없음', blank = True, null=True) #기본재료 중에서 필요한 것
     ingredients = models.TextField(max_length=300, default='') #필요한 재료
     option = models.CharField(max_length=100, default='', blank = True, null = True) #옵션
     recipy = models.TextField(default='', blank=True, null = True)  #레시피
