dc24160e8579e60ff39856b6675d3cd9740c125e
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2021-08-12 21:55:27 +0900

    fourth

--- oneportion/settings.py
+++ oneportion/settings.py
@@ -73,7 +73,7 @@ ROOT_URLCONF = 'oneportion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['oneportion/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
