eeb3766b2ab115bb29506305b971bd36c1d9995d
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2021-08-12 23:17:59 +0900

    sixth

--- oneportion/settings.py
+++ oneportion/settings.py
@@ -19,6 +19,7 @@ import cloudinary.api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -73,7 +74,7 @@ ROOT_URLCONF = 'oneportion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
